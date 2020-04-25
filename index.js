7c52017165b35d8e280eb1236dc6020a7f19b0fa
Lucas Nunes <lucasnunes000@gmail.com>
2020-04-24 23:50:34 -0300

    Configuração inicial Apollo Server e implementação de primeira Query

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hi, GraphQL!');
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        hi: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        hi() {
+            return 'Hello, GraphQL!';
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
+
+server.listen().then(({ url }) => {
+    console.log(`Server running... ${url}`);
+})
