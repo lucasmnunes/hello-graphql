c19ba502e8bcf62179b43a0dfd2636ed1b2a0731
Lucas Nunes <lucasnunes000@gmail.com>
2020-04-27 00:21:05 -0300

    Criação de DriverFilter e implementação de duas novas Mutations (excluir e alterar um Driver)

--- resolver/mutation/Driver.js
+++ resolver/mutation/Driver.js
@@ -1,8 +1,8 @@
 const { drivers, sequence } = require('../../data/data.js')
 
 module.exports = {
-    addDriver(_, { dados }) {
-        const driverExists = drivers.some(d => d.name === dados.name);
+    addDriver(_, { data }) {
+        const driverExists = drivers.some(d => d.name === data.name);
 
         if (driverExists) {
             throw new Error('Driver already exists!');
@@ -10,11 +10,60 @@ module.exports = {
 
         const driver = {
             id: sequence(),
-            ...dados
+            ...data
         }
 
         drivers.push(driver);
-
         return driver; 
+    },
+
+    deleteDriver(_, { filter }) {
+        const driverIndex = getDriverIndexByDriverFilter(filter);
+        
+        if (!isIndexValid(driverIndex)) {
+            return null;
+        }
+
+        const driversDeleted = drivers.splice(driverIndex, 1);
+        return driversDeleted ? driversDeleted[0] : null;
+    },
+
+    alterDriver(_, { filter, data}) {
+        const driverIndex = getDriverIndexByDriverFilter(filter);
+        
+        if (!isIndexValid(driverIndex)) {
+            return null;
+        }
+        
+        if (nonNull(data.name)) {
+            drivers[driverIndex].name = data.name;
+        }
+
+        if (nonNull(data.country)) {
+            drivers[driverIndex].country = data.country;
+        }       
+
+        return drivers[driverIndex];
     }
+}
+
+function getDriverIndexByDriverFilter(filter) {
+    let driverIndex = -1;
+
+    const { id, name } = filter;
+    if (id) {
+        driverIndex = drivers.findIndex(d => d.id === id);
+    } else if (name) {
+        driverIndex = driver.findIndex(d => d.name === names)
+    }
+
+    return driverIndex;
+}
+
+function isIndexValid(index) {
+    return index >= 0;
+}
+
+function nonNull(arg) {
+    return arg;
 }
